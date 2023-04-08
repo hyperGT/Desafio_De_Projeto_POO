96dc68b7abd208d1952fc5e131857cbc41d90669
hyperGT <charlesgabriel102@gmail.com>
2023-04-08 14:31:16 -0300

    Curso class

--- src/edu/gabriel/projetodesafio/bootcamp/Curso.java
+++ src/edu/gabriel/projetodesafio/bootcamp/Curso.java
@@ -1,7 +1,38 @@
 package edu.gabriel.projetodesafio.bootcamp;
 
-public class Curso {
+public class Curso extends Conteudo{
+
+    private int cargaHoraria_Curso;
 
 
+    public Curso() {
 
+    }
+
+    public Curso(int cargaHoraria_Curso) {
+        this.cargaHoraria_Curso = cargaHoraria_Curso;
+    }
+
+    public int getCargaHoraria_Curso() {
+        return cargaHoraria_Curso;
+    }
+
+    public void setCargaHoraria_Curso(int cargaHoraria_Curso) {
+        this.cargaHoraria_Curso = cargaHoraria_Curso;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo_Curso='" + getTitulo() + '\'' +
+                ", descricao_Curso='" + getDecricao() + '\'' +
+                ", cargaHoraria_Curso=" + cargaHoraria_Curso +
+                '}';
+    }
+
+    // metodo calcularXp()
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * getCargaHoraria_Curso();
+    }
 }
