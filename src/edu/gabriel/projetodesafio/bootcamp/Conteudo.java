4626dec0d20969f8eb78e2d376ac6a6d795425cd
hyperGT <charlesgabriel102@gmail.com>
2023-04-08 14:30:46 -0300

    Conteudo abstract class

---
+++ src/edu/gabriel/projetodesafio/bootcamp/Conteudo.java
@@ -0,0 +1,27 @@
+package edu.gabriel.projetodesafio.bootcamp;
+
+public abstract class Conteudo {
+
+    protected static final double XP_PADRAO = 10d;
+
+    private String titulo;
+    private String decricao;
+
+    public abstract double calcularXp();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDecricao() {
+        return decricao;
+    }
+
+    public void setDecricao(String decricao) {
+        this.decricao = decricao;
+    }
+}
