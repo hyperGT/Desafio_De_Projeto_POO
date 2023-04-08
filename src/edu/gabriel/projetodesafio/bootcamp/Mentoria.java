59c27366ff7d240b947ca4e55c72ec01edefee88
hyperGT <charlesgabriel102@gmail.com>
2023-04-08 14:31:55 -0300

    Mentoria class

--- src/edu/gabriel/projetodesafio/bootcamp/Mentoria.java
+++ src/edu/gabriel/projetodesafio/bootcamp/Mentoria.java
@@ -1,4 +1,36 @@
 package edu.gabriel.projetodesafio.bootcamp;
 
-public class Mentoria {
+import java.time.LocalDateTime;
+
+
+public class Mentoria extends Conteudo{
+    private LocalDateTime data_Mentoria;
+
+    public Mentoria() {
+    }
+
+    public LocalDateTime getData_Mentoria() {
+        return data_Mentoria;
+    }
+
+    public void setData_Mentoria(LocalDateTime data_Mentoria) {
+        this.data_Mentoria = data_Mentoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo_Mentoria='" + getTitulo() + '\'' +
+                ", descricao_Mentoria='" + getDecricao() + '\'' +
+                ", data_Mentoria=" + data_Mentoria +
+                '}';
+    }
+
+    // metodo calcularXp()
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+
 }
