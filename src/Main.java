6939b319950b22f034effafabe718c20e1c57107
hyperGT <charlesgabriel102@gmail.com>
2023-04-08 14:31:44 -0300

    Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import edu.gabriel.projetodesafio.bootcamp.Conteudo;
+import edu.gabriel.projetodesafio.bootcamp.Curso;
+import edu.gabriel.projetodesafio.bootcamp.Dev;
+import edu.gabriel.projetodesafio.bootcamp.Mentoria;
+
+import java.time.LocalDateTime;
+
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Curso cursoJava = new Curso();
+
+        Conteudo cursoPython = new Curso();
+
+        System.out.println(cursoJava.toString());
+
+        Mentoria mentoriaJava = new Mentoria();
+
+        mentoriaJava.setData_Mentoria(LocalDateTime.of(2023, 03, 28, 18, 00));
+
+
+        System.out.println(mentoriaJava.toString());
+
+
+        Dev novoUser = new Dev();
+
+        novoUser.setNomeDeUsuario_Dev("Jonas");
+        novoUser.getConteudosInscritos();
+        novoUser.getConteudosConcluidos();
+
+
+
+        System.out.println(novoUser.toString());
+
 
     }
 }
